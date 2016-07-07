38d8cf3e6e119c960ed2c34c518e41f168b6cfcf
Mary Boyd <mary.boyd314@gmail.com>
2016-07-07 00:54:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
+        if array.length == 1
+        "#{array.join(" ")}"
 
+      elsif array.length == 2
+       "#{array.insert(-2, "and").join(" ")}"
+
+     else array.insert(-2, "and")
+     "#{array.join(", ").sub("and,", "and")}"
+      end
 end
